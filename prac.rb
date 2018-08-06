fe14819767ff9d6ea32840b3e328bcbe3c7abe0b
sarahjablonski <shapeshiftwithme@gmail.com>
2018-08-06 18:18:39 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -1,3 +1,4 @@
 first_name = "Sarah"
 last_name = "Jablonski"
-puts "My name is" +"" first_name "+"last_name"
+# String Interpolation
+puts "My first name is #{first_name} and my last name is #{last_name}"
