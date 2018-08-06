1e3597bbda96a10c6a76256cae60cb94109307c1
sarahjablonski <shapeshiftwithme@gmail.com>
2018-08-06 18:22:52 +0000

    automatically backed up by learn

--- prac.rb
+++ prac.rb
@@ -2,5 +2,4 @@ first_name = "Sarah"
 last_name = "Jablonski"
 age = 14
 # String Interpolation
-puts "My first name is #{first_name} and 
-i am #{age.to_s}"
+puts "My first name is #{first_name} and i am #{age.to_s}"
